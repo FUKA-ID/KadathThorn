355916d44e3e6e45a6d4e3b81b08e31df0c01c96
Samuel Tootle <tootle@itp.uni-frankfurt.de>
2023-04-24 20:33:51 +0200

    Cleanup Add useful detect messages

--- src/detect.sh
+++ src/detect.sh
@@ -43,20 +43,17 @@ if [ -z "${KADATH_DIR}"                                                 \
 then
     echo "BEGIN MESSAGE"
     echo "Building Frankfurt University/KADATH from git repo..."
-    echo "END MESSAGE"
 
     # Set locations
-    NAME=Kadath
     SRCDIR="$(dirname $0)"
     BUILD_DIR=${SCRATCH_BUILD}/build/${THORN}
     if [ -z "${KADATH_INSTALL_DIR}" ]; then
         INSTALL_DIR=${SCRATCH_BUILD}/external/${THORN}
     else
-        echo "BEGIN MESSAGE"
-        echo "Installing Frankfurt University/KADATH into ${KADATH_INSTALL_DIR}"
-        echo "END MESSAGE"
         INSTALL_DIR=${KADATH_INSTALL_DIR}
     fi
+    echo "Installing Frankfurt University/KADATH into ${INSTALL_DIR}"
+    echo "END MESSAGE"
     KADATH_BUILD=1
     KADATH_DIR=${INSTALL_DIR}
 else
@@ -83,8 +80,11 @@ KADATH_INC_DIRS="${KADATH_DIR}/include ${KADATH_DIR}/include/Kadath_point_h"
 KADATH_LIB_DIRS="${KADATH_DIR}/lib"
 KADATH_LIBS="kadath"
 
-#KADATH_INC_DIRS="$(${CCTK_HOME}/lib/sbin/strip-incdirs.sh ${KADATH_INC_DIRS})"
-#KADATH_LIB_DIRS="$(${CCTK_HOME}/lib/sbin/strip-libdirs.sh ${KADATH_LIB_DIRS})"
+echo "BEGIN MESSAGE"
+echo "KadathThorn: Detect settings - KADATH_DIR : ${KADATH_DIR}"
+echo "KadathThorn: Detect settings - KADATH_LIB_DIRS : ${KADATH_LIB_DIRS}"
+echo "KadathThorn: Detect settings - KADATH_LIBS : ${KADATH_LIBS}"
+echo "END MESSAGE"
 
 # Pass options to Cactus
 echo "BEGIN MAKE_DEFINITION"
@@ -92,7 +92,6 @@ echo "KADATH_DIR      = ${KADATH_DIR}"
 echo "KADATH_INC_DIRS = ${KADATH_INC_DIRS}"
 echo "KADATH_LIB_DIRS = ${KADATH_LIB_DIRS}"
 echo "KADATH_LIBS     = ${KADATH_LIBS}"
-# echo "HOME_KADATH     = ${KADATH_DIR}"
 echo "END MAKE_DEFINITION"
 
 echo 'INCLUDE_DIRECTORY $(KADATH_INC_DIRS)'
